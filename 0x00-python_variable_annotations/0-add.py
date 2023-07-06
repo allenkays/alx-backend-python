f841b10d086136d275fe01ebdbf15637ac662877
allenkays <allenkays@tutanota.com>
2023-07-07 00:19:55 +0300

    Add docstrings

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,13 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+    Annotated function to add two floats and return their sum as a float.
+
+    Args:
+        a, b (float)
+
+    Returns: 
+        sum (float): returns the sum of two float args
+    """
     return a + b
