bdf2394c38bf7d3a25d5bfbbd4b319168bba3773
allenkays <allenkays@tutanota.com>
2023-07-07 01:33:06 +0300

    Adding annotation for complex list variables

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of a list of floating-point numbers.
+
+    Args:
+        input_list: The input list of floating-point numbers.
+
+    Returns:
+        The sum of the numbers as a floating-point number.
+    """
+    return sum(input_list)
