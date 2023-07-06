542f43f4ccf0890a0b21e11c14c1fe389a95299e
allenkays <allenkays@tutanota.com>
2023-07-07 01:21:15 +0300

    Add annotated function to_str that typcasts its return type to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n: The input float.
+
+    Returns:
+        The string representation of the input float.
+    """
+    return str(n)
