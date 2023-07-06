3b28a5836adcb98a3134a4f10c618b24f247bc51
allenkays <allenkays@tutanota.com>
2023-07-07 00:37:55 +0300

    Annotate function to concat two strings

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -9,9 +9,12 @@ def add(a: float, b: float) -> float:
     Annotated function to add two floats and return their sum as a float.
 
     Args:
-        a, b (float)
+        a: First floating point number
+        b: Second floating point number
 
     Returns: 
         sum (float): returns the sum of two float args
     """
     return a + b
+
+if __name__ == '__main__':
---
+++ 1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Args:
+        str1: The first string.
+        str2: The second string.
+
+    Returns:
+        The concatenation of the two input strings.
+    """
+    return str1 + str2
