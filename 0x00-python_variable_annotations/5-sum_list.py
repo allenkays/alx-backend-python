46839d6b5d42a7e56d10ceb369abebd4f83295cc
allenkays <allenkays@tutanota.com>
2023-07-07 16:13:29 +0300

    Add module documentation and add another blank line after imports

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to return the sum of a list of floats
+"""
 
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     """
