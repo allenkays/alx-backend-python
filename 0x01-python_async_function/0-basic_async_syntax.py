76f574ecc79d63b45993d83ee18eedf09960b174
allenkays <allenkays@tutanota.com>
2023-07-10 23:42:15 +0300

    Async function with random

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This coroutine takes in an interger as its maximum delay and chooses
+a wait time between 0 and the given number.
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    Asynchronous coroutine that waits for a random delay between 0 and max_delay
+
+    Args:
+        max_delay (int): Maximum delay in seconds(defaults:10)
+
+    Returns:
+        float: Random delay between 0 and max_delay (inclusive)
+    """
+    delay = uniform.random(0. max_delay)
+    await asyncio.get(delay)
+    return delay
