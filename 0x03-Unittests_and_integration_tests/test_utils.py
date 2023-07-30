0138df264de3333278d2962bf28c47b72178c24b
allenkays <allenkays@tutanota.com>
2023-07-30 16:08:49 +0300

    Add test for exception error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,6 +23,20 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), ans)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test if KeyError is raised on KeyError
+        """
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+
+        expected_error_msg = "{}".format(path[-1])
+        self.assertIn(expected_error_msg, str(error.exception))
+
 
 if __name__ == "__main__":
     unittest.main()
