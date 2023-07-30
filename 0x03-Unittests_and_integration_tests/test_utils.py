dde91aab653286ae4e3a4a2be34d0544946bd379
allenkays <allenkays@tutanota.com>
2023-07-30 15:38:58 +0300

    Add parameterized test case

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Unit tests module
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    class to test if utils.access_nested_map  returns what it is
+    supposed to return
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a"), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, ans):
+        """
+        Assert test
+        """
+        self.assertEqual(access_nested_map(nested_map, path), ans)
+
+
+if __name__ == "__main__":
+    unittest.main()
